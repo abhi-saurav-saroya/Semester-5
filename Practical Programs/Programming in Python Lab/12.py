4e3592b2c47fb1eabc6d53a59fdfece32e4fad91
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-09-10 23:07:08 +0530

    feat(programs/pip): add program 12

---
+++ Practical Programs/Programming in Python Lab/12.py
@@ -0,0 +1,16 @@
+def factorial(n: int) -> int:
+    '''
+        Function to calculate factorial of an integer.
+    '''
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Factorial is not defined for negative numbers")
+else:
+    print("Factorial of", num, "is:", factorial(num))
