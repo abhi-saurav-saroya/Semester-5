e79e9a6c94aa294ec7abe00eefce66a14be5bafb
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-09-10 22:58:50 +0530

    feat(programs/pip): add program 10

---
+++ Practical Programs/Programming in Python Lab/10.py
@@ -0,0 +1,11 @@
+# Increasing number of stars
+for i in range(1, 5):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+# Decreasing number of stars
+for i in range(3, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
