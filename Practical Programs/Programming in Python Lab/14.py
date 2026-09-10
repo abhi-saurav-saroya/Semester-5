d5ade074d7f453d8d08dd63ff6d72086298ead53
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-09-10 23:20:13 +0530

    feat(programs/pip): add program 14

---
+++ Practical Programs/Programming in Python Lab/14.py
@@ -0,0 +1,10 @@
+# Program to import and use the Fibonacci module
+
+import fibonacci as fibo
+
+# Taking input from the user
+n = int(input("Enter the number of terms: "))
+
+# Calling the function from the module
+print("Fibonacci series:")
+fibo.fibonacci(n)
