15948a282b33bfc28d0eadf1fdc73e7aedfa59f4
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-09-13 22:08:19 +0530

    feat(programs/java): add program 3

---
+++ Practical Programs/Java Programming Lab/PrimeNumbers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class PrimeNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println("Prime numbers from 1 to " + num + " are:");
+
+        for (int i = 2; i <= num; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
