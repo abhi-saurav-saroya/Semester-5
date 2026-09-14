411ebefacb92f23d102bd67e417489f22e2501d1
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-09-14 21:42:58 +0530

    feat(programs/stur): add program 1

---
+++ Practical Programs/Statistical Computation Techniques using R/1.R
@@ -0,0 +1,20 @@
+age <- 21.5
+cat("Numeric:", age, "\n")
+
+marks <- 85L
+cat("Integer:", marks, "\n")
+
+name <- "Abhi"
+cat("Character:", name, "\n")
+
+is_student <- TRUE
+cat("Logical:", is_student, "\n")
+
+z <- 3 + 2i
+cat("Complex:", z, "\n")
+
+numbers <- c(10, 20, 30, 40)
+cat("Numeric Vector:", numbers, "\n")
+
+names <- c("Abhi", "Rahul", "Aman")
+cat("Character Vector:", names, "\n")
